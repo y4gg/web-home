378ff78748010cf88211e26fdcf679d9dbee223e
ysflkc <yusuflukic@icloud.com>
2025-06-19 16:39:19 +0200

    Before trieng shit

--- src/app/buttons.tsx
+++ src/app/buttons.tsx
@@ -1,1 +1,11 @@
+"use client"
 import { useSession } from "next-auth/react";
+
+export default function Buttons() {
+  const { data: session } = useSession();
+  return (
+    <div>
+      <p>Signed in as {session?.user?.email}</p>
+    </div>
+  );
+}
--- src/components/buttons.tsx
+++ src/components/buttons.tsx
@@ -1,10 +1,4 @@
 "use client";
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -19,7 +13,6 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useState } from "react";
 import { signIn, SessionProvider } from "next-auth/react";
-import Buttons from "./buttons"
 
 export default function Home() {
   const [username, setUsername] = useState("");
@@ -28,12 +21,6 @@ export default function Home() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold text-white">Web Home</h1>
-      <p className="text-lg text-gray-300">
-        Teleport back to your minecraft home over this website
-      </p>
-      <div className="flex flex-row gap-4 mt-6">
         <Dialog>
           <form>
             <DialogTrigger asChild>
@@ -66,41 +53,5 @@ export default function Home() {
             </DialogContent>
           </form>
         </Dialog>
-      </div>
-      <Accordion
-        type="single"
-        className="w-80 mt-6"
-        // defaultValue="item-1"
-        collapsible
-      >
-        <AccordionItem value="item-1">
-          <AccordionTrigger className="text-white">
-            How does this work?
-          </AccordionTrigger>
-          <AccordionContent className="text-white">
-            After you register, you can simply click the button to teleport to
-            your home - after setting it up first of course.
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-2">
-          <AccordionTrigger className="text-white">
-            Are there any cooldowns?
-          </AccordionTrigger>
-          <AccordionContent className="text-white">
-            Yes, there are cooldowns. You can only teleport every 5 minutes, and
-            you can only change your home once per day.
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-3">
-          <AccordionTrigger className="text-white">
-            How do I register?
-          </AccordionTrigger>
-          <AccordionContent className="text-white">
-            After you register, a link will be sent to your chat, and yes you
-            have to be online.
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
-    </div>
   );
 }
