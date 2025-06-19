a72951a102ef57364f65885cbd15159cd7023ad1
ysflkc <yusuflukic@icloud.com>
2025-06-19 14:26:57 +0200

    Before Custom Mail

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
-import { prisma } from "./prisma"
+import { prisma } from "@/prisma"
  
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
