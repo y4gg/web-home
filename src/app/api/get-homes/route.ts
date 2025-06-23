6e0e74ba4d77c2ed4ce04cbeee66df07e470cc51
ysflkc <yusuflukic@icloud.com>
2025-06-23 10:17:01 +0200

    Added a get-homes route for future use

---
+++ src/app/api/get-homes/route.ts
@@ -0,0 +1,35 @@
+import { auth } from "@/auth";
+
+export async function GET(request: Request) {
+  const session = await auth();
+  if (!session) {
+    return new Response(
+      JSON.stringify({ success: false, error: "Unauthorized" }),
+      { status: 401 }
+    );
+  }
+  const identifier = session.user?.email;
+  const { prisma } = await import("@/prisma");
+
+  // Find user by email (identifier)
+  const user = await prisma.user.findUnique({ where: { email: identifier! } });
+  if (!user) {
+    return new Response(
+      JSON.stringify({ success: false, error: "User not found" }),
+      { status: 404 }
+    );
+  }
+
+  const homes = await prisma.home.findMany({
+    where: {
+      userId: user.id,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return new Response(JSON.stringify({ success: true, homes }), {
+    status: 200,
+  });
+}
